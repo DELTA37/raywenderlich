8bc9cfeea893e00599da9367d2987240771d1fb5
George <gkasparyants@gmail.com>
2020-02-24 10:52:14 +0300

    layout swiftui

---
+++ layout_swiftui/layout_swiftui/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  layout_swiftui
+//
+//  Created by Георгий Каспарьянц on 24.02.2020.
+//  Copyright © 2020 Георгий Каспарьянц. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Text("Hello, World!")
+            Divider()
+            Text("high")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ table_views/table_views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  table_views
+//
+//  Created by Георгий Каспарьянц on 24.02.2020.
+//  Copyright © 2020 Георгий Каспарьянц. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
